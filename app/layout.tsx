57f3a83a9e0822b1f27f41143901c1e96ec37454
tiniakosn <tiniakosn@gmail.com>
2026-01-23 14:58:01 +0000

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,11 +45,11 @@ export const metadata: Metadata = {
   // 2. Ρύθμιση για το Favicon (Browser Tab)
   icons: {
     icon: [
-      { url: "/favicon-v3.png", sizes: "32x32" },
-      { url: "/favicon-v3.png", sizes: "16x16" },
+      { url: "/favicon-v4.png", sizes: "32x32" },
+      { url: "/favicon-v4.png", sizes: "16x16" },
     ],
     apple: [
-      { url: "/apple-touch-icon-v2.png", sizes: "180x180", type: "image/png" },
+      { url: "/apple-touch-icon-v4.png", sizes: "180x180", type: "image/png" },
     ],
   },
 
