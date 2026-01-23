ee02cf36ee6adfb7ba1da535e8f2f5818c7679d2
tiniakosn <tiniakosn@gmail.com>
2026-01-23 10:01:57 +0000

    ai

--- app/api/itinerary/route.ts
+++ app/api/itinerary/route.ts
@@ -4,6 +4,9 @@ import { GoogleGenerativeAIStream, StreamingTextResponse } from "ai";
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY!);
 
 export async function POST(req: Request) {
+
+  console.log("LOGGING KEY START:", process.env.GOOGLE_GENERATIVE_AI_API_KEY?.substring(0, 7));
+  console.log("LOGGING KEY LENGTH:", process.env.GOOGLE_GENERATIVE_AI_API_KEY?.length);
   try {
     const { days, style, lang } = await req.json();
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
