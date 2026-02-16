e2776ec3e060f26b171454a0a17576732e0b1184
tiniakosn <tiniakosn@gmail.com>
2026-02-16 09:30:28 +0000

    seo

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://androsguesthouses.gr';
+  const baseUrl = 'https://www.androsguesthouses.gr';
 
   const routes = [
     '', // Αρχική
